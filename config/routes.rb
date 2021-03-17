7577c2759ff76e2b24b8012915ba3e06b10e1040
Miles Miller <mmille60@binghamton.edu>
2021-03-17 16:36:15 +0000

    so it doesn't break

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'movies#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   resources :movies
   
+  root 'movies#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
